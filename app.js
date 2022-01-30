07e30e7027824dc97815ad57a8734c7bd39abf51
Your Name <mohammadalhamdan94@gmail.com>
2022-01-30 12:42:31 +0300

    task 2 done

--- app.js
+++ app.js
@@ -1,13 +1,37 @@
 const express = require("express");
+const res = require("express/lib/response");
 
 const app = express();
-const products = require("./products");
+app.use(express.json()); //middleware
+let products = require("./products");
 
 
-app.listen(8000, () => {
-    console.log("The application is running on localhost:8000");
-  });
 
   app.get("/products", (req, res) => {
-    res.json(products);
+    res.json(req.body);
+  });
+
+  app.post("/products",(req,res) =>{
+      const id = products.length +1;
+      const newProduct = {id:id, ...req.body};
+      products.push(newProduct);
+      res.status(201);
+      res.json(newProduct)
+  })
+
+  app.delete("/products/:productId",(req,res) =>{
+      const {productId} = req.params;
+      const foundProduct = products.find(product => product.id ===+productId)
+      if(foundProduct){
+          const newArray = products.filter((product) => product.id !== +productId);
+          products = newArray
+          res.status(204).end();
+          res.json(newArray)
+
+      }else{
+        res.status(404).end();
+      }
+  })
+  app.listen(8000, () => {
+    console.log("The application is running on localhost:8000");
   });
